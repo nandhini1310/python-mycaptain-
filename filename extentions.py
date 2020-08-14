079aa515b45e42aec4ac04b867c46d2db222e67f
nandhini1310 <69678681+nandhini1310@users.noreply.github.com>
2020-08-14 05:10:26 -0700

    Add files via upload

---
+++ filename extentions.py
@@ -0,0 +1,5 @@
+a= input ("enter the filename:")
+if a=="abc.py" :
+    print("the extension of the file is :  python")
+else:
+    print (" not valid extention")
---
+++ radius.py
@@ -0,0 +1,3 @@
+from math import pi
+r= float(input("enter the radiua"))
+print ("the area of the circle is:" + str(pi*r**2))
